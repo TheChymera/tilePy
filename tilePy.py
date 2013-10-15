e2c264548839182d7b4a40253083732c0a1df2fb
Horea Christian <h.chr@mail.ru>
2013-10-15 05:37:15 +0200

    renamed sequence, added comments

--- tilePy.py
+++ tilePy.py
@@ -9,23 +9,23 @@ from PIL import Image
 from os import listdir, path, makedirs
 
 image_dir = path.dirname(path.dirname(path.realpath(__file__))) + '/faceRT/experiment/img/px6/'
-sequence = pd.DataFrame.from_csv('/home/chymera/src/pystim/sequences/faceRT_blocksize4.csv')
+sequence = pd.DataFrame.from_csv('/home/chymera/src/pystim/sequences/faceRT.csv')
 output_dir = path.dirname(path.realpath(__file__)) + '/output/'
 
 if not path.isdir(output_dir):
     makedirs(output_dir)
 
 for idx, trial in sequence.iterrows():
-    bg = Image.new('L', (1360,768), (122))
+    bg = Image.new('L', (1360,768), (122)) #size of the background, in pixels AND pixel color value
     top_img = Image.open(image_dir+trial['top face'],'r')
     top_img = top_img.resize((int(top_img.size[0]//10*7),int(top_img.size[1]//10*7)), Image.ANTIALIAS) # Use PIL to resize :(
     left_img = Image.open(image_dir+trial['left face'],'r')
     left_img = left_img.resize((int(left_img.size[0]//10*7),int(left_img.size[1]//10*7)), Image.ANTIALIAS) # Use PIL to resize :(
     right_img = Image.open(image_dir+trial['right face'],'r')
     right_img = right_img.resize((int(right_img.size[0]//10*7),int(right_img.size[1]//10*7)), Image.ANTIALIAS) # Use PIL to resize :(
-    bg.paste(top_img,(565,89))
-    bg.paste(left_img,(269,385))
-    bg.paste(right_img,(861,385))
-    bg.save(path.splitext(output_dir+trial['top face'])[0]+'_composite'+'.jpg')
+    bg.paste(top_img,(565,89)) #coordinates of the top left corner where the image is pasted
+    bg.paste(left_img,(269,385)) #idem
+    bg.paste(right_img,(861,385)) #idem
+    bg.save(path.splitext(output_dir+trial['top face'])[0]+'_em'+str(trial['emotion intensity'])+'_composite'+'.bmp')
     #~imgplot = plt.imshow(bg, cmap = cm.Greys_r, vmin=0, vmax=255, interpolation='nearest')
     #~plt.show()
